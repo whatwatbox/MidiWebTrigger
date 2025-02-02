b9cfecad45476cdb259b2bb724ec2379adff09af
WATBOX <whatwatbox+github@gmail.com>
2025-02-03 01:39:11 +0900

    更新対応
    clusterの仕様に対応
    近い色を返す関数の処理をHSV基準へ変更

--- classes/TriggerButton.js
+++ classes/TriggerButton.js
@@ -1,20 +1,59 @@
 class TriggerButton
 {
-    static getTriggerButton(parentElement)
-    {
-        const button = parentElement.querySelector("button");
-        //子のp要素を取得する
-        const p = button.querySelector("p");
-        //p要素のテキストを取得する
-        const name = p.textContent;
-        //p要素のtriggercolor属性を取得する
-        const color = p.getAttribute("triggercolor");
+    // static getTriggerButton(parentElement)
+    // {
+    //     const button = parentElement.querySelector("button");
+    //     //子のp要素を取得する
+    //     const p = button.querySelector("p");
+    //     //p要素のテキストを取得する
+    //     const name = p.textContent;
+    //     //p要素のtriggercolor属性を取得する
+    //     const color = p.getAttribute("triggercolor");
 
+    //     return new TriggerButton(
+    //         name,
+    //         color,
+    //         button
+    //     );
+    // }
+
+    static getTriggerButton(parentElement) {
+        const button = parentElement.querySelector("button");
+        // 子のp要素を取得する
+        const p = button.querySelector("p");
+        // p要素のテキストを取得する
+        const name = p.textContent;
+    
+        // 親要素を取得する
+        var buttonParent = button.parentElement;
+        // 親要素のcomputed styleからbackground-colorを取得する
+        const computedStyle = window.getComputedStyle(buttonParent);
+        const bgColor = computedStyle.backgroundColor;
+    
+        // 変換したカラーコード
+        const color = this.rgbToHex(bgColor);
+    
         return new TriggerButton(
             name,
             color,
             button
         );
+    }
+
+    // rgbまたはrgba表記を16進数表記(例："FFFFFF")に変換する関数
+    static rgbToHex(rgb) {
+        // "rgb(255, 255, 255)" または "rgba(255, 255, 255, 1)" の形式に対応
+        const result = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)/);
+        if (!result) return null;
+        const r = parseInt(result[1], 10);
+        const g = parseInt(result[2], 10);
+        const b = parseInt(result[3], 10);
+        // 各値を2桁の16進数文字列に変換し連結
+        return (
+            ("0" + r.toString(16)).slice(-2) +
+            ("0" + g.toString(16)).slice(-2) +
+            ("0" + b.toString(16)).slice(-2)
+        ).toUpperCase();
     }
 
     //初期化
